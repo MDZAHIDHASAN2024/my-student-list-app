645747716774d4c58f6ac3b114e70422912894ae
MDZAHIDHASAN2024 <zahidbinanwar3@gmail.com>
2025-06-20 22:26:02 +0600

    third commit

--- src/components/UseReducer/UseRuducer.jsx
+++ src/components/UseReducer/UseRuducer.jsx
@@ -1,15 +1,17 @@
-import React, { useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 
 const sutdentList = [
-  { id: 101, name: 'jahid', subject: 'One' },
-  { id: 102, name: 'Sharif', subject: 'Two' },
-  { id: 103, name: 'Anamul', subject: 'Nine' },
-  { id: 104, name: 'Hanif', subject: 'Three' },
+  { id: 101, name: 'jahid', subject: 'One', roll: '01' },
+  { id: 102, name: 'Sharif', subject: 'Two', roll: '02' },
+  { id: 103, name: 'Anamul', subject: 'Nine', roll: '03' },
+  { id: 104, name: 'Hanif', subject: 'Three', roll: '04' },
 ];
 
 const UseReducer = () => {
   const [students, setStudents] = useState('');
   const [subject, setSubject] = useState('');
+  const [roll, setRoll] = useState('');
+  const [search, setSearch] = useState('');
   const reducer = (state, action) => {
     if (action.type === 'ADD') {
       const studentAll = [...state.name, action.payload];
@@ -31,6 +33,16 @@ const UseReducer = () => {
         modalText: 'Student is Removed',
       };
     }
+    if (action.type === 'SEARCH') {
+      const searched = [...state.name].filter((searchValue) => {
+        const value = searchValue.name.toLowerCase();
+        return value.startsWith(action.payload.toLowerCase());
+      });
+      return {
+        ...state,
+        name: searched,
+      };
+    }
     return state;
   };
 
@@ -48,10 +60,12 @@ const UseReducer = () => {
       id: new Date().getTime().toString(),
       name: students,
       subject: subject,
+      roll: roll,
     };
     dispatch({ type: 'ADD', payload: studentNew });
     setStudents('');
     setSubject('');
+    setRoll('');
   };
 
   const Modal = ({ modalText }) => {
@@ -61,14 +75,24 @@ const UseReducer = () => {
   const handleRemove = (id) => {
     dispatch({ type: 'REMOVE', payload: id });
   };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    dispatch({ type: 'SEARCH', payload: search });
+  }, [search]);
   return (
     <div>
       <h2>My Student App (first time)</h2>
-      <form onSubmit={handleAddStudent}>
+      <form className="border" onSubmit={handleAddStudent}>
         <div className="mb-3">
           <label htmlFor="name">
             Name:
             <input
+              placeholder="Enter Your Name"
               type="text"
               value={students}
               onChange={(e) => setStudents(e.target.value)}
@@ -80,9 +104,22 @@ const UseReducer = () => {
           <label htmlFor="subject">
             Class:
             <input
+              placeholder="Enter Your Class"
               type="text"
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="roll">
+            Roll No:
+            <input
+              placeholder="Enter Your Roll"
+              type="text"
+              value={roll}
+              onChange={(e) => setRoll(e.target.value)}
               required
             />
           </label>
@@ -91,16 +128,26 @@ const UseReducer = () => {
           Add Student
         </button>
       </form>
+
       {studentListState.isModalOpen && (
         <Modal modalText={studentListState.modalText} />
       )}
+
+      <input
+        className="mt-3"
+        placeholder="Search Student"
+        value={search}
+        onChange={handleSearch}
+      />
+
       {studentListState.name.map((student) => {
-        const { id, name, subject } = student;
+        const { id, name, subject, roll } = student;
         return (
           <div key={id} className="border m-2 bg-secondary text-light ">
-            <li className="border">ID: {id}</li>
+            <li className="border ">ID: {id}</li>
             <li className="border">Name: {name}</li>
             <li className="border">Class: {subject}</li>
+            <li className="border">Roll No: {roll}</li>
             <button
               className="btn btn-danger"
               onClick={() => {
